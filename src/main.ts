171828631a8eb96818ed8a495cf5c552520d87fa
japago25andes <j.pardog@uniandes.edu.co>
2024-02-27 18:00:20 -0500

    02-objests-interface

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+import './topics/01-basic-types.ts';
+
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `Hola mundo`
+
+console.log("hola")
---
+++ src/topics/01-basic-types.ts
@@ -0,0 +1,7 @@
+const name: string = 'strider';
+let hpPoints: number | 'FULL' = 100;
+const isAlive: boolean = true;
+
+hpPoints = 'FULL'; // valid
+
+console.log({name, hpPoints, isAlive});
