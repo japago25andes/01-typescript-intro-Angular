d04918fb96839d80d342db9dfc0c3a535fbd0e26
japago25andes <j.pardog@uniandes.edu.co>
2024-02-28 18:28:53 -0500

    06-function-destructuring

---
+++ src/topics/06-function-destructuring.ts
@@ -0,0 +1,53 @@
+interface Product {
+    description: string;
+    price: number;
+}
+
+const phone: Product = {
+    description: 'Samsung Galaxy S21',
+    price: 150
+}
+
+const tablet: Product = {
+    description: 'iPad Pro',
+    price: 250
+}
+
+interface TaxCalculationOptions {
+    tax: number;
+    products: Product[];
+}
+
+
+//function taxCalculation(options: TaxCalculationOptions): number[] {
+//function taxCalculation({tax,products}: TaxCalculationOptions): number[] {
+function taxCalculation(options: TaxCalculationOptions): number[] {
+
+    const {tax, products} = options;
+
+    let total = 0;
+
+    products.forEach( ({price}) => {
+        total += price;
+    });
+
+    return [total, total * tax];
+
+}
+
+
+const shoppingCart = [phone, tablet];
+
+const tax = 0.15;
+
+const [total, taxTotal] = taxCalculation({
+    products: shoppingCart,
+    tax
+});
+
+
+console.log('Total: ', total);
+console.log('Tax: ', taxTotal);
+
+
+export { }
