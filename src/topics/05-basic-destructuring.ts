3741dd651aed0f8239cb8cbad83d34ffceba632d
japago25andes <j.pardog@uniandes.edu.co>
2024-02-28 16:49:27 -0500

    05-basic-destructuring

---
+++ src/topics/05-basic-destructuring.ts
@@ -0,0 +1,57 @@
+interface AudioPlayer {
+    audioVolume: number;
+    songDuration: number;
+    song: string;
+    details: Details;
+}
+
+interface Details {
+    autor: string;
+    year: number;
+}
+
+
+const audioPlayer: AudioPlayer = {
+    audioVolume: 90,
+    songDuration: 36,
+    song: 'Mess',
+    details: {
+        autor: 'Ed Sheeran',
+        year: 2005
+    }
+}
+
+
+const song = 'New song';
+
+const { song:anotherSong, songDuration:duration} = audioPlayer;
+//const { details: { autor } } = audioPlayer;
+const {autor:autor} = audioPlayer.details;
+
+
+//console.log('Song: ', anotherSong);
+//console.log('Duration: ', duration);
+//console.log('Autor: ', autor);
+
+
+const dbz: string[] = ['Goku', 'Vegeta', 'Trunks'];
+const trunks = dbz[3] || 'No existe';
+
+
+console.error('Personaje: ', trunks);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {}
