40d78eeb5789150b00757871c39d9feb464d9ac5
japago25andes <j.pardog@uniandes.edu.co>
2024-02-28 21:09:20 -0500

    10-decorators

---
+++ src/topics/10-decorators.ts
@@ -0,0 +1,30 @@
+function classDecorator(){
+
+} 
+    
+
+@classDecorator
+export class SuperClass{
+    public myProperty: string = 'Hello';
+
+    print(){
+        console.log('Hello');
+    }
+}
+
+
+
+
+
+console.log(SuperClass);
+
+const myClass = new SuperClass();
+
+console.log(myClass);
+
+
+
+
+
+
+
